7e0427a851f289e897db8b55d611224c034c5542
pancake <63361401+KaqRadostinova@users.noreply.github.com>
2022-10-04 17:18:40 +0300

    Add project files.

---
+++ TestGoogleSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestGoogleSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestGoogleSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f495803-b2e9-4f53-b11f-5ca484d8d643")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestGoogleSearch/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace TestGoogleSearch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSearchAndDisplayStreetFighterFive()
+        {
+            int waitingTime = 1000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]");
+            By googleResultText = By.XPath(".//h2/span[text()='Street Fighter V']");
+            By googleAcceptAllButton = By.Id("L2AGLb");
+
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleAcceptAllButton).Click();
+
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+            webDriver.Manage().Window.Maximize();
+
+            var actualResultText = webDriver.FindElement(googleResultText);
+
+            Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+
+            webDriver.Quit();
+        }
+    }
+}
